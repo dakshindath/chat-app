e975a2872ec83fd37800671d6e73347dd535e18b
dakshindath <dakshindath4@gmail.com>
2025-05-10 17:41:13 +0530

    first commit

--- routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getMessages } = require('../controllers/messageController.js');
-const router = express.Router();
-
-router.get('/', getMessages);
-
-module.exports = router;
